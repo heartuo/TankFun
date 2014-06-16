f155d0be66f1b0529e4dac42ec7129bc48688b17
heartuo <wenscontact@gmail.com>
2014-06-16 04:15:22 -0500

    just some testing

--- src/OvalPanelCanvas.java
+++ src/OvalPanelCanvas.java
@@ -1,3 +1,5 @@
+import java.awt.image.BufferedImage;
+
 import java.awt.Color;
 import java.awt.Graphics;
 import javax.swing.JFrame;
@@ -14,6 +16,8 @@ public class OvalPanelCanvas extends JPanel {
 	    int width = getWidth();
 	    int height = getHeight();
 	    g.setColor(Color.black);
+	    g.fillRect(0, 0, width, height);
+	    g.setColor(Color.white);
 	    g.drawOval(0, 0, width, height);
 	    g.drawString("Hello world! ", 75, 75);
 	  }
@@ -28,6 +32,15 @@ public class OvalPanelCanvas extends JPanel {
 	    frame.add(new OvalPanelCanvas());
 	    frame.setSize(300, 200);
 	    frame.setVisible(true);
+	    
+	    BufferedImage image = ImageConverter.readBufferedImage("assets/images/watch_dog.png");
+	    int[][] test = ImageConverter.to2DArray(image);
+	    for (int j = 0; j < test[0].length; j++) {
+	    	for (int i = 0; i < test.length; i++) {
+	    		System.out.printf("%X ", test[i][j]);
+	    	}
+	    	System.out.println();
+	    }
 	}
 
 }
